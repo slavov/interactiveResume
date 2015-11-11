9c39e5594e75320e8f512d02496ff79b1661f979
Georgi Slavov <gslavov@sdl.com>
2015-11-11 17:04:23 +0200

    bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,20 @@
-var name = "Georgi Slavov";
-var formattedName = HTMLheaderName.replace("%data%", name);
 
-var role = "Java Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var skills = ["Java ", "Spring Core, ", "Spring MVC, ", "JS, ", "jQuery "];
+var bio = {
+    "name" : "Georgi Slavov",
+    "role" : "Java Developer",
+    "contacts" : {
+        "mobile" : "+380636645939",
+        "email" : "mr.george.slavov@gmail.com",
+        "github" : "slavov",
+        "twitter" : "g_slavov",
+        "location" : "Ukraine. Kiev"
+    },
+    "pic" : "images/georgi.png",
+    "age" : 29,
+    "skills" : skills
+};
 
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").prepend(formattedRole).prepend(formattedName);
-
-
